d7231b15a385daddf78b3534e385ce3e7ad2250a
jeisonoliver <jeisonjs42@hotmail.com>
2021-01-23 00:17:21 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,50 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///                  Projeto Sistema de Gestão Escolar                      ///
+///                Developed by  @jeisonoliver - Sep, 2021                  ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void menuSobre(void);
+
+int main(void) {
+    menuSobre();
+    return 0;
+
+}
+
+
+void menuSobre(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///               Projeto de Controle de Cheques Recebidos                  ///\n");
+    printf("///                  Developed by  @jeisonoliver, 2021                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///      = = = = = Sistema de Controle de Cheques Recebidos = = = = =       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///   Programa feito para a disciplina DCT1106 – Programação. O presente    ///\n");
+    printf("///   programa tem o objetivo de iniciar os trabalhos referentes à          ///\n");
+    printf("///   disciplina de programação, este programa é o ponta pé inicial para o  ///\n");
+    printf("///   projeto que terá duração de 15 semanas, o projeto de nome SIG-Check   ///\n");
+    printf("///   tem como objetivo realizar o cadastro e o acompanhamento de cheques   ///\n");
+    printf("///   pré-datados, auxiliando o controle financeiro da empresa.             ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///               DESENVOLVEDOR: JEISON DE OLIVEIRA AZEVEDO                 ///\n");
+    printf("///                    EMAIL: jeisonjs42@gmail.com                          ///\n");
+    printf("///                  DISCIPLINA: DCT1106 – PROGRAMAÇÃO                      ///\n");
+    printf("///                 PROFESSOR:  FLAVIUS DA LUZ E GORGONIO                   ///\n");
+    printf("///                          SEMESTRE: 2020.2                               ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+} 
