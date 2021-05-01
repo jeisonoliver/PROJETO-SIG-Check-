a73744de1b7cc2e21a594c714c70e3519f3a3da4
jeisonoliver <jeisonjs42@gmail.com>
2021-04-30 22:02:25 -0300

    Implementando Relatorios

--- relatorios.c
+++ relatorios.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "relatorios.h"
+
+typedef struct recebido Recebido;
+
 
 void telaRealtorio1(void) {
 	int Vcheque1;
@@ -95,37 +100,12 @@ void telaRealtorio2(void) {
 
 
 void telaRealtorio3(void) {
-    system("clear");
-	printf("\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///             ||||||||||||||||||||||||||||||||||||||||||||              ///\n");
-	printf("///             | Sistema de Controle de Cheques Recebidos |              ///\n");
-	printf("///             |            SISTEMA SIG-CHECK             |              ///\n");
-	printf("///             ||||||||||||||||||||||||||||||||||||||||||||              ///\n");
-	printf("///                                                                       ///\n");
-	printf("///                Developed by  @jeisonoliver - Jan, 2021                ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///            ||||||||||||||||||||||||||||||||||||||||||||               ///\n");
-	printf("///            |         PROPOSTA DE RELATORIO 3          |               ///\n");
-	printf("///            ||||||||||||||||||||||||||||||||||||||||||||               ///\n");
-	printf("///                                                                       ///\n");
-	printf("///                  Proposta de Reatorio numero 3:                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("///           lista de cheques recebidos de uma empresa ou de um          ///\n");
-	printf("///           cliente a partir do nome do numero da conta do mesmo(a).    ///\n");
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-	getchar();
+	Recebido *listaRec;
+	listaRec = NULL;
+	gerarRelatorio(&listaRec);
+	exibirLista(listaRec);
 }
+
 
 void telaRealtorio4(void) {
     system("clear");
@@ -155,6 +135,80 @@ void telaRealtorio4(void) {
 	printf("///                                                                       ///\n");
 	printf("///                                                                       ///\n");
 	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+void apagarLista(Recebido **listaRec)
+{
+    Recebido *rec;
+    
+    while (*listaRec != NULL)
+    {
+   	 rec = *listaRec;
+   	 *listaRec = (*listaRec)->prox;
+   	 free(rec);
+    }
+    printf("Lista excluida com sucesso! \n");    
+}
+
+void gerarRelatorio(Recebido **listaRec)
+{
+    FILE *fp;
+    Recebido *rec;
+    
+    apagarLista(&(*listaRec));
+    *listaRec = NULL;
+    fp = fopen("recebidos.dat","rb");
+    if (fp == NULL){
+   		printf("Erro na abertura do arquivo... \n");
+   		exit(1);
+    }
+    else{
+   	rec = (Recebido *) malloc(sizeof(Recebido));
+   		while (fread(rec, sizeof(Recebido), 1, fp)){
+        	if ((*listaRec == NULL) || (strcmp(rec->numero, (*listaRec)->numero) < 0)) {
+        	rec->prox = *listaRec;
+        	*listaRec = rec;
+        	} else  {
+          	Recebido* ant = *listaRec;
+          	Recebido* atu = (*listaRec)->prox;
+          		while ((atu != NULL) && (strcmp(atu->numero, rec->numero) < 0)) {
+            	ant = atu;
+            	atu = atu->prox;
+          		}
+          	ant->prox = rec;
+          	rec->prox = atu;
+        	}
+        rec = (Recebido *) malloc(sizeof(Recebido));
+   	 }
+   	 free(rec);
+   	 printf("Arquivo recuperado com sucesso! \n");
+   	 fclose(fp);
+    }    
+}
+
+void exibirLista(Recebido *aux)
+{
+printf("\n\n");
+printf("****************************************\n");
+printf("*** Relatorio dos Alunos Cadastrados ***\n");
+printf("****************************************\n");
+printf("| Numero |\t| Nome do banco |\t| Agencia |\t| Conta |\t| valor |\t| Data Recebimento |\t| Data Vencimento|\n");
+printf("\n");
+	while (aux != NULL)
+	{
+    	printf("  %s\t\t",aux->numero);
+    	printf("%s\t\t",aux->nomeBanco);
+    	printf("%s-%s\t\t",aux->agencia, aux->digitoA);
+		printf("%s-%s\t\t",aux->numeroConta, aux->digitoC);
+		printf("%d,%d\t\t\t",aux->valorReal, aux->valorCentavos);
+		printf("%s\t\t",aux->dataR);
+		printf("%s\n",aux->dataV);
+    	aux = aux->prox;
+	}
+	printf("\nFim da Lista! \n\n");
 	printf("\n");
 	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
 	getchar();
--- relatorios.h
+++ relatorios.h
@@ -1,3 +1,20 @@
+struct recebido{	
+	char numero[5];
+	char nomeBanco[51];
+	char agencia[10];
+	char numeroConta[12];
+	int valorReal;
+	int valorCentavos;
+	char dataR[12];
+	char dataV[12];
+	char digitoA[2];
+	char digitoC[2];
+	int status;
+    struct recebido *prox;
+};
+
+typedef struct recebido Recebido;
+
 void telaRealtorio1(void);
 
 void telaRealtorio2(void);
@@ -5,3 +22,10 @@ void telaRealtorio2(void);
 void telaRealtorio3(void);
 
 void telaRealtorio4(void);
+
+void gerarRelatorio(Recebido **listaRec);
+
+void apagarLista(Recebido **listaRec);
+
+void exibirLista(Recebido *aux);
+
